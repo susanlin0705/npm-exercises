a0a0be01d88d3c3f0df8e03a08e4428db2c16cb3
susanlin0705 <susan.lin0705@gmail.com>
2019-07-19 09:47:45 -0500

    add bonus

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@ const sayHello = () => console.log('hello');
 sayHello("Hello");
 
 
-$("body").css("background-color","green");
+$('body').css("background-color","yellow");
+// const get = require('simple-get')
+//
+// get('http://example.com', function (err, res) {
+//     if (err) throw err
+//     console.log(res.statusCode) // 200
+//     res.pipe(process.stdout) // `res` is a stream
+//     window.location = 'http:example.com'
+// })
+
