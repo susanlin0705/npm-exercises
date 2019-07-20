e7d83e1aba85cd72a9f3ca63ae0fedb2064370a3
susanlin0705 <susan.lin0705@gmail.com>
2019-07-20 09:58:11 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-const $ = require('jquery');
-const sayHello = () => console.log('hello');
-const pokemon = require('pokemon');
-
-sayHello("Hello");
-
-
-$('body').css("background-color","yellow");
-// const get = require('simple-get')
+// const $ = require('jquery');
+// const sayHello = () => console.log('hello');
+// const pokemon = require('pokemon');
+// const pokemonGif = require('pokemon-gif');
 //
-// get('http://example.com', function (err, res) {
-//     if (err) throw err
-//     console.log(res.statusCode) // 200
-//     res.pipe(process.stdout) // `res` is a stream
-//     window.location = 'http:example.com'
-// })
+// sayHello("Hello");
+//
+//
+// $('body').css("background-color","yellow");
+// // const get = require('simple-get')
+// //
+// // get('http://example.com', function (err, res) {
+// //     if (err) throw err
+// //     console.log(res.statusCode) // 200
+// //     res.pipe(process.stdout) // `res` is a stream
+// //     window.location = 'http:example.com'
+// // })
+// ;
+// console.log(pokemon.all());
+// //=> ['Bulbasaur', …]
+// $("body").text(pokemonGif('pikachu'));
+// console.log(pokemon.random());
+// $("body").text(pokemon.random());
+// //=> 'Snorlax'
+//
+// pokemon.getName(147);
+// //=> 'Dratini'
+//
+// pokemon.getId('Dratini');
+// //=> 147
+//
+import niceMessage from "./say-hello.js";
 
-console.log(pokemon.all());
-//=> ['Bulbasaur', …]
-
-console.log(pokemon.random());
-$("body").text(pokemon.random());
-//=> 'Snorlax'
-
-pokemon.getName(147);
-//=> 'Dratini'
-
-pokemon.getId('Dratini');
-//=> 147
-
+console.log(niceMessage());
+// const hello = require('./say-hello.js');
+// console.log(hello())
---
+++ src/say-hello.js
@@ -0,0 +1,12 @@
+export const niceMessage = () => {
+    return "Hello Welcome to my page.";
+};
+
+export default niceMessage
+
+// module.exports=hello;
+// const hello = function(){
+//     return "Hello"
+// };
+//
+// module.exports=hello
